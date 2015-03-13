832519260ae7c30e53427f312db0c9448a6ecd32
GrumpyDane <micklerche@gmail.com>
2015-03-13 09:19:34 -0500

    Initial Commit

---
+++ MM_Assessment_1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MM_Assessment_1
+//
+//  Created by Mick Lerche on 3/13/15.
+//  Copyright (c) 2015 Mick Lerche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MM_Assessment_1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MM_Assessment_1
+//
+//  Created by Mick Lerche on 3/13/15.
+//  Copyright (c) 2015 Mick Lerche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
